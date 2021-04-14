89bc177f7a4ce27b17a2a0e2e075ee37e46a04b8
beautidata <beautifuldatadev@gmail.com>
2021-04-13 20:13:49 -0500

    add several working methods, string method and chart method do not work

--- budget-app/budget.py
+++ budget-app/budget.py
@@ -1,7 +1,59 @@
 class Category:
+    def __init__(self, name):
+        self.name = name
+        self.ledger = []
+        self.balance = 0.00
 
+    def deposit(self, amount, description=''):
+        self.ledger.append({"amount": amount, "description": description})
+        self.balance += amount
 
+    def withdraw(self, amount, description=''):
+        if self.check_funds(amount) is True:
+            self.ledger.append({"amount": (amount * -1), "description": description})
+            self.balance -= amount
+            return True
+        else:
+            return False
+
+    def get_balance(self):
+        return self.balance
+
+    def get_name(self):
+        return self.name
+
+    def get_ledger(self):
+        return self.ledger
+
+    def transfer(self, amount, category):
+        if category.check_funds(amount) is True:
+            self.deposit(amount, f"Transfer from {category.get_name()}")
+        return category.withdraw(amount, f"Transfer to {self.get_name()}")
+
+    def check_funds(self, amount):
+        if self.get_balance() >= amount:
+            return True
+        else:
+            return False
+
+    def __str__(self):
+        mystring = ""
+        star_num = int((30 - len(self.get_name())) // 2)
+        for n in range(star_num):
+            mystring += "*"
+            mystring += self.get_name()
+        while len(mystring) < 30:
+            mystring += "*"
+        mystring += "\n"
+        for x in range(len(self.get_ledger())):
+            mystring += self.ledger[x]['description']
+            mystring += " "
+            mystring += str(self.ledger[x]['amount'])
+            mystring += "\n"
+        mystring += f"Total: {self.get_balance()}"
+        return mystring
 
 
 
 def create_spend_chart(categories):
+    print('spend chart created.')
